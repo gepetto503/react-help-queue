5ceff2394bbb5514bf6c8b769f2540b5d7491a66
Brendan Hanna <gepetto503@gmail.com>
2017-08-22 22:56:47 -0700

    nest ticket component in ticketlist in app.  up to the end of components lesson

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Ticket(props){
+  return (
+    <div>
+      <h3>3a</h3>
+      <h3>Bumbaclot</h3>
+      <p><em>Firebase won't save record</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default Ticket;
