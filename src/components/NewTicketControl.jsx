ff78f1930f4650c33faf2d2729f2313f9c7daf5c
Brendan Hanna <gepetto503@gmail.com>
2017-08-23 10:08:00 -0700

    form displays on button click.  up to handling form submission section of conditional rendering etc. lesson

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,5 +1,6 @@
-//imports react library
+//imports
 import React from "react";
+import NewTicketForm from "./NewTicketForm";
 
 //establishes react component with class name
 class NewTicketControl extends React.Component {
@@ -8,31 +9,33 @@ class NewTicketControl extends React.Component {
   //establishes beginning state
   constructor(props) {
    super(props);
-   this.state = {formVisibleOnPage: false};
+   this.state = {formIsVisibleOnPage: false};
    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this);
   }
 
   //our event handler method for when the button is clicked
   handleDisplayingNewTicketForm(event){
-    this.setState({formVisibleOnPage: true});
-    console.log(this);
+    this.setState({formIsVisibleOnPage: true});
+    console.log(this.state);
   }
 
   //what to render in reacts virtual dom
   render(){
+    const formIsVisibleOnPage = this.state.formIsVisibleOnPage;
     let formAreaContent = null;
-    if (this.state.formVisibleOnPage) {
-      formAreaContent = <h1>Form will eventually go here!</h1>
+    if (formIsVisibleOnPage){
+      formAreaContent = <NewTicketForm/>
     } else {
-      formAreaContent = <button onClick={this.handleDisplayingNewTicketForm.bind(this)}>Request Help</button>;
+       formAreaContent = <button onClick={this.handleDisplayingNewTicketForm.bind(this)}>Request Help</button>;
     }
+
     return (
       <div>
         {formAreaContent}
       </div>
     );
   }
+
 }
 
-//only one thing is exported: the class NewTicketControl
 export default NewTicketControl;
