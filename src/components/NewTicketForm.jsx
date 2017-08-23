60c12d5a2b0731178b68ef12523b6b75ec1024d0
Brendan Hanna <gepetto503@gmail.com>
2017-08-23 13:44:27 -0700

    refactor component tree, add queue component

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 
-function NewTicketForm(props) {
-  return (
-    <div>
-      <form>
-        <input
-          type="text"
-          id="names"
-          placeholder="Pair Names"/>
-        <input
-          type="text"
-          id="location"
-          placeholder="Location"/>
-        <textarea
-          id="issue"
-          placeholder="Describe your issue."/>
-        <button type="submit">Help!</button>
-      </form>
-    </div>
-  );
+class NewTicketForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleNewTicketFormSubmission = this.handleNewTicketFormSubmission.bind(this);
+  }
+
+  handleNewTicketFormSubmission(event) {
+    event.preventDefault()
+    const {_names, _location, _issue} = this.refs;
+    alert('babalaboo! ${_names.value}, ${_location.value}, ${_issue.value}');
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleNewTicketFormSubmission}>
+          <input
+            ref="_names"
+            type="text"
+            id="names"
+            placeholder="Pair Names"/>
+          <input
+            ref="_location"
+            type="text"
+            id="location"
+            placeholder="Location"/>
+          <textarea
+            ref="_issue"
+            id="issue"
+            placeholder="Describe your issue."/>
+          <button type="submit">Help!</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewTicketForm;
